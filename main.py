8a4c56424001d86fa3191e1644cda12377efeede
Leon Ochsenfeld <ochsenfeld@hotmail.com>
2022-05-19 00:16:04 +0200

    initial

---
+++ main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+from processing.processor import Processor
+import matplotlib.pyplot as plt
+
+
+def test():
+    plt.figure(figsize=(9, 3))
+    plt.grid(True)
+    plt.plot([68,
+              244,
+              531,
+              784,
+              958,
+              1218,
+              1425
+              ], [472,
+                  674,
+                  990,
+                  1272,
+                  1473,
+                  1777,
+                  2020
+                  ], "--bo")
+    plt.show()
+
+
+def main():
+    test()
+    return
+    processor = Processor()
+    path = "D:/Projekte/macki/Kalibrierung 17.05/Kranstellungen 17.05/2022-05-17_16-23-42.mp4"
+    processor.analyze(path, "16:24:09")
+
+    path = "D:/Projekte/macki/Kalibrierung 17.05/Kranstellungen 17.05/2022-05-17_16-25-42.mp4"
+    processor.analyze(path, "16:25:55")
+
+    path = "D:/Projekte/macki/Kalibrierung 17.05/Kranstellungen 17.05/2022-05-17_16-25-42.mp4"
+    processor.analyze(path, "16:26:30")
+
+    path = "D:/Projekte/macki/Kalibrierung 17.05/Kranstellungen 17.05/2022-05-17_16-28-42.mp4"
+    processor.analyze(path, "16:29:06")
+
+    path = "D:/Projekte/macki/Kalibrierung 17.05/Kranstellungen 17.05/2022-05-17_16-29-42.mp4"
+    processor.analyze(path, "16:29:49")
+
+    path = "D:/Projekte/macki/Kalibrierung 17.05/Kranstellungen 17.05/2022-05-17_16-30-42.mp4"
+    processor.analyze(path, "16:31:11")
+
+    path = "D:/Projekte/macki/Kalibrierung 17.05/Kranstellungen 17.05/2022-05-17_16-31-42.mp4"
+    processor.analyze(path, "16:31:53")
+
+    print("running main")
+
+
+if __name__ == "__main__":
+    main()
---
+++ processing/processor.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+import cv2
+from ctypes import Array
+import numpy as np
+import os
+import datetime
+
+
+class Processor:
+
+    def get_center_of_mass(self, frame: Array) -> None:
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        lower1 = np.array([169, 150, 20])
+        upper1 = np.array([180, 255, 255])
+
+        mask = cv2.inRange(hsv, lower1, upper1)
+
+        # lower2 = np.array([0, 120, 20])
+        # upper2 = np.array([12, 255, 255])
+
+        # mask2 = cv2.inRange(hsv, lower2, upper2)
+
+        # mask = cv2.bitwise_or(mask1, mask2)
+
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        points = []
+        if len(contours) != 0:
+            for contour in contours:
+                if cv2.contourArea(contour) > 20:
+                    x, y, w, h = cv2.boundingRect(contour)
+                    # cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 255), 1)
+                    cX = int(x+w/2)
+                    cY = int(y+h/2)
+                    points.append((cX, cY))
+                    cv2.circle(frame, (cX, cY), 30, [255, 255, 255])
+                    # print(cX,cY)
+                    # cv2.putText(frame, "center", (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        cv2.imshow('frame', frame)
+        cv2.imshow('Output', mask)
+        return points
+
+    def analyze(self, filePath: str, timestamp: str) -> None:
+        cap = cv2.VideoCapture(filePath)
+        c_time = os.path.basename(filePath)
+        start_time = datetime.datetime.strptime(c_time, '%Y-%m-%d_%H-%M-%S.mp4')
+        print(start_time)
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        print("width",width)
+        last_time = 0.0
+        while cap.isOpened():
+            # Capture frame-by-frame
+            frame_exists, curr_frame = cap.read()
+            if frame_exists:
+                frame = curr_frame[510:600, 0:-1]
+                points = self.get_center_of_mass(frame)
+                frame_timestamp = start_time + \
+                    datetime.timedelta(milliseconds=last_time)
+                last_time = last_time+1000/fps
+
+                if frame_timestamp == datetime.datetime.strptime("2022-05-17_"+timestamp, '%Y-%m-%d_%H:%M:%S'):
+                    point = list(points)[0]
+                    print(frame_timestamp, point, (point[0]/width)*100)
+                    # return
+                # Display the resulting frame
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    break
+
+            # Break the loop
+            else:
+                break
+        # When everything done, release the video capture object
+        cap.release()
+        # Closes all the frames
+        cv2.destroyAllWindows()
