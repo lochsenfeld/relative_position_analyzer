c457d58da1b0729ac40a485ad305e102d52a5a39
Leon Ochsenfeld <ochsenfeld@hotmail.com>
2022-06-09 21:24:44 +0200

    fix: processing

--- processing/processor.py
+++ processing/processor.py
@@ -23,8 +23,6 @@ class Processor:
 
         lower1 = np.array([170, 20, 20])
         upper1 = np.array([180, 255, 255])
-        # lower1 = np.array([h_low, s_low, v_low])
-        # upper1 = np.array([h_high, s_high, v_high])
 
         mask = cv2.inRange(hsv, lower1, upper1)
 
@@ -56,93 +54,7 @@ class Processor:
             print("MORE THAN 2 POINTS")
             # cv2.imwrite("test.png", frame)
         return points
-        # lower1 = np.array([h_low, s_low, v_low])
-        # upper1 = np.array([h_high, s_high, v_high])
 
-        mask1 = cv2.inRange(hsv, lower1, upper1)
-
-        lower2 = np.array([169, 150, 20])
-        upper2 = np.array([180, 255, 255])
-
-        mask2 = cv2.inRange(hsv, lower2, upper2)
-
-        mask = cv2.bitwise_or(mask1, mask2)
-
-        lower3 = np.array([172, 0, 136])
-        upper3 = np.array([173, 41, 144])
-        mask3 = cv2.inRange(hsv, lower3, upper3)
-
-        mask = cv2.subtract(mask, mask3)
-
-        lower4 = np.array([173, 40, 101])
-        upper4 = np.array([178, 52, 127])
-        mask4 = cv2.inRange(hsv, lower4, upper4)
-
-        mask = cv2.subtract(mask, mask4)
-
-        kernel = (3, 3)
-        # mask = cv2.erode(mask, kernel, iterations=cv2.getTrackbarPos("i1", "trackbars"))
-        # mask = cv2.dilate(mask, kernel, iterations=cv2.getTrackbarPos("i2", "trackbars"))
-        normed = cv2.normalize(mask, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
-        kernel = cv2.getStructuringElement(shape=cv2.MORPH_ELLIPSE, ksize=(3, 3))
-        mask = cv2.morphologyEx(normed, cv2.MORPH_OPEN, kernel)
-
-        contours, _ = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-
-        points = []
-
-        for contour in contours:
-            if cv2.contourArea(contour) > 20:
-                # x, y, w, h = cv2.boundingRect(contour)
-                # cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 1, 16)
-                rbox = cv2.minAreaRect(contour)
-                (cX, cY), (w, h), rot_angle = rbox
-                if rot_angle <= 80 and rot_angle >= 100:
-                    continue
-                points.append((int(cX), int(cY)))
-        if len(points) == 0:
-            cs = list(map(lambda x: cv2.contourArea(x), contours))
-            # if len(cs) > 0:
-            #     print("KEINE DATENPUNKTE!", max(cs))
-            # else:
-            #     print("KEINE DATENPUNKTE!")
-        elif len(points) > 2:
-            print("MORE THAN 2 POINTS")
-            # cv2.imwrite("test.png", frame)
-        return points
-
-    def test(self):
-
-        # c2 = cv2.VideoCapture("D:/Projekte/macki/Kalibrierung 17.05/Kranstellungen 17.05/2022-05-17_22-59-41.mp4")
-        # _, f2 = c2.read()
-        # f2 = f2[510:600, 0:-1]
-        cv2.namedWindow("trackbars")
-        cv2.resizeWindow("trackbars", 1900, 200)
-
-        def empty(x):
-            pass
-        # 160,180,40,148,105,219
-
-        # lower1 = np.array([160, 40, 105])
-        # upper1 = np.array([180, 148, 219])
-        cv2.createTrackbar("h_low", "trackbars", 160, 180, empty)
-        cv2.createTrackbar("h_high", "trackbars", 180, 180, empty)
-        cv2.createTrackbar("s_low", "trackbars", 40, 255, empty)
-        cv2.createTrackbar("s_high", "trackbars", 148, 255, empty)
-        cv2.createTrackbar("v_low", "trackbars", 105, 255, empty)
-        cv2.createTrackbar("v_high", "trackbars", 219, 255, empty)
-        cv2.createTrackbar("i1", "trackbars", 3, 10, empty)
-        cv2.createTrackbar("i2", "trackbars", 4, 10, empty)
-        while True:
-            f1 = cv2.imread("test.png")
-            m1 = self.get_center_of_mass(f1)
-            cv2.imshow('f1', f1)
-            cv2.imshow('m1', m1)
-
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
-        # Closes all the frames
-        cv2.destroyAllWindows()
 
     @staticmethod
     def get_file_path_for_timestamp(path, timestamp) -> str:
